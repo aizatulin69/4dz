498aa7d2ae70419baf1af290fa0c250942ee84e8
aizatulin69 <kirilajzatulin@gmail.com>
2025-08-04 03:25:48 +0300

    Update and rename 4.4.py to 4dot4.py

---
+++ 4dot4.py
@@ -0,0 +1,85 @@
+all_contacts = []
+
+
+def hallo():
+    print('Hallo, how can I help you?')
+
+
+def add_contact(comand_input):
+    is_repeat = False
+    text = comand_input.strip().split(' ')
+    name = text[1]
+    contact = text[2]
+    if not contact.isdigit():
+        raise ValueError
+    for contact_dict in all_contacts:
+        if name in contact_dict:
+            is_repeat = True
+            break
+    if not is_repeat:
+        all_contacts.append({name: contact})
+        print('Contact added.')
+    else:
+        print('Contact already exists.')
+
+
+def change_contact(comand_input):
+    text = comand_input.strip().split(' ')
+    name = text[1]
+    new_contact = text[2]
+    for i in range(len(all_contacts)):
+        if name in all_contacts[i]:
+            all_contacts[i][name] = new_contact
+            print("Contact changed.")
+            return
+    raise KeyError
+
+
+def show_phone(comand_input):
+    text = comand_input.strip().split(' ')
+    name = text[1]
+    for contact in all_contacts:
+        if name in contact:
+            print(contact[name])
+            return
+    raise KeyError
+
+
+def show_all():
+    print(all_contacts)
+
+
+def close():
+    print("Good bye!")
+
+
+def main():
+
+    while True:
+        comand = input(">>> ").strip().lower()
+
+        action = comand.strip().split(' ')[0]
+
+        if action.lower() in ('hallo', 'hi'):
+            hallo()
+        elif action.lower() == 'add':
+            add_contact(comand)
+        elif action.lower() == 'change':
+            change_contact(comand)
+        elif action.lower() == 'phone':
+            show_phone(comand)
+        elif action.lower() == 'all':
+            show_all()
+        elif action.lower() in ('close', 'exit', 'bye'):
+            close()
+            return False
+        else:
+            print('Please, enter another command')
+            
+
+if __name__ == "__main__":
+    main()
+
+            
+
+        
